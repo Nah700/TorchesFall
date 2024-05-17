d0ea4a3d36b67c6a0f2c5bdbdb769c62e77d4f60
Lopotiron <julien.dufaur@epitech.eu>
2024-05-17 18:14:37 +0200

    fix: fixed jump possible when not in ground

--- Player.gd
+++ Player.gd
@@ -1,14 +1,28 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-# Called when the node enters the scene tree for the first time.
+var speed = 5.0
+var gravity = -100
+var jump_force = 15.0
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("move_left", "move_right")
-	if Input.is_action_just_pressed("jump"):
-		input.y += 15
+	handle_movement(delta)
+
+func handle_movement(delta):
+	var input_dir = Vector3.ZERO
+	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir = input_dir.normalized()
 	
-	apply_central_force(input * 1200.0 * delta)
+	var direction = (transform.basis * input_dir).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_force
+	move_and_slide()
